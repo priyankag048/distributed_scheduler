eb59d1d88761a01f82c56b13ff4e7a07fb5c96ee
Priyanka Guha <priyankaguha.2012@gmail.com>
2017-08-24 13:43:32 +0530

    connectionpool changes and test cases

---
+++ test/jobTest.js
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = "test";
+/* eslint-disable */
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../lib/index");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Jobs",()=>{
+    describe("Create Job",()=>{
+        it("it should POST a job",(done)=>{
+            let job = {
+                id : 1,
+                description : "New Job",
+                frequency : 14,
+                script : "let a = 3"
+            }
+
+            chai.request(server)
+            .post('/scheduler/create')
+            .send(job)
+            .end((err,res)=>{
+                res.should.have.status(201);
+                res.body.should.be.a("object");
+                res.body.should.have.property("info");
+                res.body.should.have.property("info").eql("Record has been successfully created with id 1");
+            });
+        })
+    })
+})
