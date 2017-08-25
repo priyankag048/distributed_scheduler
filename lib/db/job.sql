3de8d377e5b81dd5e7daa3e970041a77fb78a2b4
Priyanka Guha <priyankaguha.2012@gmail.com>
2017-08-25 19:12:04 +0530

    new changes

--- lib/db/job.sql
+++ lib/db/job.sql
@@ -1,8 +1,20 @@
 CREATE TABLE job(
-    id int primary key,
-    description text,
+    id serial not null,
+    name char(10) not null,                 
+    description text,                   
     frequency int not null,
     startdate text not null,
     script text not null,
-    pid int references process(id)
+    pid int references process(id),
+    primary key(id, name)
 )
+
+
+COMMENT ON COLUMN job.id IS "Unique job id created sequentially : Primary Key";
+COMMENT ON COLUMN job.name IS "Unique job name given by the end user : Primary Key";
+COMMENT ON COLUMN job.description IS "Description of the job created by the end user";
+COMMENT ON COLUMN job.frequency IS "Frequency of job: the rate at which the job will run";
+COMMENT ON COLUMN job.startdate IS "Time Stamp when the job is created";
+COMMENT ON COLUMN job.script IS "The script that is supposed to run when the job is scheduled";
+COMMENT ON COLUMN job.pid IS "The process to which the job will be assigned. This column has a foreign key relation with column process.id";
+
--- lib/db/process.sql
+++ lib/db/process.sql
@@ -6,3 +6,11 @@ CREATE TABLE process(
     rss_to_cpu int,
     assigned_jobs_count int
 )
+
+
+COMMENT ON COLUMN process.id IS "Unique process id created when the process starts: Primary Key";
+COMMENT ON COLUMN process.port IS "Port on which the process runs";
+COMMENT ON COLUMN process.rss IS "Memory Usage : RSS( Resident Set Size ) by the process";
+COMMENT ON COLUMN process.cpu_utilization IS "CPU utilization of the process";
+COMMENT ON COLUMN process.rss_to_cpu IS "RSS to CPU utilization ratio, used to determine how many jobs can be assigned";
+COMMENT ON COLUMN process.assigned_jobs_count IS "Number of jobs assigned to the process";
